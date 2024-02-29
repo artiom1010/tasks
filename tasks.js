39879d3272ecd64026d81968501249c20f70315b
artiom212 <sary.artema18@gmail.com>
2024-02-29 18:35:59 +0200

    first commit

---
+++ tasks.js
@@ -0,0 +1,18 @@
+function twoSum(nums = [7, 2, 2, 1, 4, 3], target = 8) {
+
+    const map = new Map(); 
+  
+    for (let i = 0; i < nums.length; i++) {
+      const number = target - nums[i];
+  
+      if (map.has(number)) {
+        return [map.get(number), i];
+      }
+  
+      map.set(nums[i], i);
+    }
+  
+    return null; 
+  }
+  
+  console.log(twoSum()); 
