aabb5e780aa51ed734dde0b0235731de0e59c816
artiom1010 <160424319+artiom1010@users.noreply.github.com>
2024-04-19 13:57:59 +0300

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -95,7 +95,7 @@ function findRepeatingNums(arrayNum) {
     
     return duplicates;
 }
-
+// TO DO
 const array = [2, 1, 2, 1, 43, 3, 342, 242424, 342, 342];
 
 console.log(findRepeatingNums(array));
